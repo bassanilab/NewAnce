fa10322fd4d7821bc6e186d1a243369d10af0049
mmueller <Kurfuersten_20>
2020-03-13 16:09:30 +0100

    New Software Licence text

--- src/main/java/newance/util/ExecutableOptions.java
+++ src/main/java/newance/util/ExecutableOptions.java
@@ -1,3 +1,13 @@
+/*
+Copyright (C) SIB - Swiss Institute of Bioinformatics, Lausanne, Switzerland
+Copyright (C) LICR - Ludwig Institute of Cancer Research, Lausanne, Switzerland
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation, either version 2 of the License, or (at your option) any later version.
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with this program. If not, see https://www.gnu.org/licenses/.
+*/
+
 package newance.util;
 
 import org.apache.commons.cli.*;
